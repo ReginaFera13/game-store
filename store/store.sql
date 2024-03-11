eabd6406cb114cca53654b7b86c55742809d2917
ReginaFera13 <shawnmariesmith@icloud.com>
2024-03-11 15:25:16 -0400

    tables created and loaded

--- store/store.sql
+++ store/store.sql
@@ -1,49 +1,47 @@
--- Drop the table if it exists
 DROP TABLE IF EXISTS game;
 
--- Create Game Table
 CREATE TABLE game (
     game_id INT PRIMARY KEY,
-    game_title VARCHAR(255) UNIQUE NOT NULL CHECK (game_title ~ '^[A-Za-z0-9 _\-:''\\]+$'),
-    quantity INT NOT NULL CHECK(quantity > 0 AND quantity < 51),
-    price DECIMAL(5, 2) NOT NULL CHECK(price > 10 AND price < 60)
+    game_title VARCHAR(100) UNIQUE NOT NULL,
+    quantity INT NOT NULL,
+    price DECIMAL(5, 2) NOT NULL
 );
-
-
-
--- Insert Sample Data into Game Table
-\COPY game FROM './data/game.csv' WITH CSV HEADER;
-
 
 DROP TABLE IF EXISTS action_figure;
 
 CREATE TABLE action_figure (
     id INT PRIMARY KEY,
-    action_figure_title VARCHAR,
-    quantity INT,
-    price DECIMAL(5,2)
+    action_figure_title VARCHAR(100) UNIQUE NOT NULL,
+    quantity INT NOT NULL,
+    price DECIMAL(5, 2) NOT NULL
 );
-
-\COPY action_figure FROM './data/action_figure.csv' WITH CSV HEADER;
 
 DROP TABLE IF EXISTS employee;
 
 CREATE TABLE employee (
     id INT PRIMARY KEY,
-    employee_name VARCHAR,
-    position VARCHAR,
-    salary DECIMAL(7,2)
+    employee_name VARCHAR(100) NOT NULL,
+    position VARCHAR(100) NOT NULL,
+    salary DECIMAL(7, 2) NOT NULL
 );
-
-\COPY employee FROM './data/employee.csv' WITH CSV HEADER;
 
 DROP TABLE IF EXISTS poster;
 
 CREATE TABLE poster (
     id INT PRIMARY KEY,
-    poster_title VARCHAR,
-    quantity INT,
-    price DECIMAL(4,2)
+    poster_title VARCHAR(100) NOT NULL,
+    quantity INT NOT NULL,
+    price DECIMAL(4, 2) NOT NULL
 );
 
-\COPY poster FROM './data/poster.csv' WITH CSV HEADER;
+-- \COPY game
+-- FROM '/home/reginafera13/codeplatoon/game-store-III/store/data/game.csv' WITH CSV HEADER;
+
+-- \COPY action_figure
+-- FROM '/home/reginafera13/codeplatoon/game-store-III/store/data/action_figure.csv' WITH CSV HEADER;
+
+-- \COPY poster
+-- FROM '/home/reginafera13/codeplatoon/game-store-III/store/data/poster.csv' WITH CSV HEADER;
+
+-- \COPY employee
+-- FROM '/home/reginafera13/codeplatoon/game-store-III/store/data/employee.csv' WITH CSV HEADER;
