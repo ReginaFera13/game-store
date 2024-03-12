d3d404e6e2f5dda22f998d103815ee320589d14c
ReginaFera13 <shawnmariesmith@icloud.com>
2024-03-12 14:47:09 -0400

    added tables for genre, genre_game, gaming_engine, social security and shifts and set up the foreign key relashionships

--- store/store.sql
+++ store/store.sql
@@ -1,37 +1,118 @@
-DROP TABLE IF EXISTS game;
+DROP TABLE IF EXISTS genre CASCADE;
+
+CREATE TABLE genre (
+    genre_id SERIAL PRIMARY KEY,
+    genre_name VARCHAR(50)
+        UNIQUE
+        NOT NULL
+        CHECK (genre_name ~ '^[A-Z][A-Za-z ]+$')
+);
+
+DROP TABLE IF EXISTS gaming_engine CASCADE;
+
+CREATE TABLE gaming_engine (
+    engine_id SERIAL PRIMARY KEY,
+    engine_name VARCHAR(100)
+        UNIQUE
+        NOT NULL
+        CHECK (engine_name ~ '^[A-Z][A-Za-z0-9 ]+$')
+);
+
+DROP TABLE IF EXISTS game CASCADE;
 
 CREATE TABLE game (
-    game_id INT PRIMARY KEY,
-    game_title VARCHAR(100) UNIQUE NOT NULL,
-    quantity INT NOT NULL,
-    price DECIMAL(5, 2) NOT NULL
+    game_id SERIAL PRIMARY KEY,
+    engine_id INT,
+    game_title VARCHAR(255) 
+        UNIQUE 
+        NOT NULL 
+        CHECK (game_title ~ '^[A-Z][A-Za-z0-9 _\-:''\\]+$'),
+    quantity INT 
+        NOT NULL 
+        CHECK(quantity > 0 AND quantity < 51),
+    price DECIMAL(4, 2) 
+        NOT NULL 
+        CHECK(price > 10 AND price < 60),
+    FOREIGN KEY (engine_id) REFERENCES gaming_engine(engine_id)
+);
+
+DROP TABLE IF EXISTS genre_game;
+
+CREATE TABLE genre_game (
+    genre_game_id SERIAL PRIMARY KEY,
+    game_id INT,
+    genre_id INT,
+    FOREIGN KEY (game_id) REFERENCES game (game_id),
+    FOREIGN KEY (genre_id) REFERENCES genre (genre_id)
 );
 
 DROP TABLE IF EXISTS action_figure;
 
 CREATE TABLE action_figure (
-    id INT PRIMARY KEY,
-    action_figure_title VARCHAR(100) UNIQUE NOT NULL,
-    quantity INT NOT NULL,
-    price DECIMAL(5, 2) NOT NULL
+    action_figure_id SERIAL PRIMARY KEY,
+    action_figure_title VARCHAR(100) 
+        UNIQUE 
+        NOT NULL
+        CHECK (action_figure_title ~ '^[A-Z][A-Za-z0-9 \-]+$'),
+    quantity INT 
+        NOT NULL
+        CHECK(quantity > 0 AND quantity < 31),
+    price DECIMAL(5, 2) 
+        NOT NULL
+        CHECK (price > 10 AND price < 100.01)
 );
 
-DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS employee CASCADE;
 
 CREATE TABLE employee (
-    id INT PRIMARY KEY,
-    employee_name VARCHAR(100) NOT NULL,
-    position VARCHAR(100) NOT NULL,
-    salary DECIMAL(7, 2) NOT NULL
+    employee_id SERIAL PRIMARY KEY,
+    employee_name VARCHAR(100) 
+    NOT NULL
+    CHECK (employee_name ~ '^[A-Z][A-Za-z ]+$'),
+    position VARCHAR(100) 
+        NOT NULL
+        CHECK (position IN ('Sales Associate', 'Store Manager', 'Inventory Clerk', 'Customer Service Representative', 'IT Specialist', 'Marketing Coordinator', 'Assistant Manager', 'Finance Analyst', 'Security Officer', 'HR Coordinator')),
+    salary DECIMAL(7, 2) 
+        NOT NULL
+        CHECK (salary > 31987.19 AND salary < 60000.01)
 );
 
 DROP TABLE IF EXISTS poster;
 
 CREATE TABLE poster (
-    id INT PRIMARY KEY,
-    poster_title VARCHAR(100) NOT NULL,
-    quantity INT NOT NULL,
-    price DECIMAL(4, 2) NOT NULL
+    poster_id SERIAL PRIMARY KEY,
+    poster_title VARCHAR(100) 
+        UNIQUE 
+        NOT NULL
+        CHECK (poster_title ~ '^[A-Z][A-Za-z0-9 \-]+$'),
+    quantity INT 
+        NOT NULL
+        CHECK(quantity > 0 AND quantity < 31),
+    price DECIMAL(4, 2) 
+        NOT NULL
+        CHECK(price > 6 AND price < 20)
+);
+
+DROP TABLE IF EXISTS social_security;
+
+CREATE TABLE social_security (
+    social_security_id SERIAL PRIMARY KEY,
+    employee_id INT,
+    ssn VARCHAR(11)
+        UNIQUE
+        NOT NULL
+        CHECK (ssn ~ '^\d{3}-\d{2}-\d{4}$'),
+    FOREIGN KEY (employee_id) REFERENCES employee(employee_id)
+);
+
+DROP TABLE IF EXISTS shifts;
+
+CREATE TABLE shifts (
+    shift_id SERIAL PRIMARY KEY,
+    start_time TIMESTAMP NOT NULL,
+    end_time TIMESTAMP NOT NULL,
+    employee_id int,
+    FOREIGN KEY (employee_id) REFERENCES employee(employee_id)
 );
 
 -- \COPY game
@@ -45,3 +126,18 @@ CREATE TABLE poster (
 
 -- \COPY employee
 -- FROM '/home/reginafera13/codeplatoon/game-store-III/store/data/employee.csv' WITH CSV HEADER;
+
+-- \COPY gaming_engine
+-- FROM '/home/reginafera13/codeplatoon/game-store-III/store/data/gaming_engine.csv' WITH CSV HEADER;
+
+-- \COPY genre_game
+-- FROM '/home/reginafera13/codeplatoon/game-store-III/store/data/genre_game.csv' WITH CSV HEADER;
+
+-- \COPY genre
+-- FROM '/home/reginafera13/codeplatoon/game-store-III/store/data/genre.csv' WITH CSV HEADER;
+
+-- \COPY shifts
+-- FROM '/home/reginafera13/codeplatoon/game-store-III/store/data/shifts.csv' WITH CSV HEADER;
+
+-- \COPY social_security
+-- FROM '/home/reginafera13/codeplatoon/game-store-III/store/data/social_security.csv' WITH CSV HEADER;
